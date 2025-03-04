88fe300f00fd978da985bf6464215ab721293806
Sonia <soniadevpro@gmail.com>
2025-03-04 10:24:58 +0100

    init du projet v2

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,24 +1,3 @@
-# backend/apps/accounts/views.py
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from .models import User
-from .serializers import UserSerializer, UserCreateSerializer
+from django.shortcuts import render
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return UserCreateSerializer
-        return UserSerializer
-    
-    def get_permissions(self):
-        if self.action == 'create':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
-    
-    @action(detail=False, methods=['get'])
-    def me(self, request):
-        serializer = self.get_serializer(request.user)
-        return Response(serializer.data)
+# Create your views here.
